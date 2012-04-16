3ff2cd196e2d5d7d312a8fa356400c9ca4c1b620
Kelly Egan <kelly.egan@gmail.com>
2012-04-15 21:09:58 -0400

    Added cvCalcOpticalFlowBM skeleton

--- src/ofxOpticalFlowBM.cpp
+++ src/ofxOpticalFlowBM.cpp
@@ -32,4 +32,16 @@ void ofxOpticalFlowBM :: setup( int width, int height ) {
 
 void ofxOpticalFlowBM :: update( ofImage& source ) {
   
+  /*
+  cvCalcOpticalFlowBM(<#const CvArr *prev#>,  //CvArr * - 
+                      <#const CvArr *curr#>,  //CvArr * - 
+                      <#CvSize block_size#>,  //CvSize  - 
+                      <#CvSize shift_size#>,  //CvSize  - 
+                      <#CvSize max_range#>,   //CvSize  - 
+                      <#int use_previous#>,   //int     - 
+                      <#CvArr *velx#>,        //CvArr   - 
+                      <#CvArr *vely#>         //CvArr   - 
+  );
+  */
+  
 }
